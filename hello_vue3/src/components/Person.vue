df694f584d8b700fd0a52594e3b64705191c17b9
Kuan_wsl2 <roumiankuan@outlook.com>
2026-01-02 10:08:05 +0800

    19， watch监视情况三——reactive定义的对象类型

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="person">
-    <h1>情况二：监视ref定义的对象类型数据</h1>
+    <h1>情况三：监视reactive定义的对象类型数据</h1>
     <h2>姓名：{{ person.name }}</h2>
     <h2>年龄：{{ person.age }}</h2>
     <button @click="changeName">修改名字</button>
@@ -10,35 +10,34 @@
 </template>
 
 <script setup lang="ts">
-import { watch, ref } from 'vue'
+import { watch, reactive } from 'vue'
 
-let person = ref({
+let person = reactive({
   name: '张三',
   age: 18,
 })
 
 const changeName = () => {
-  person.value.name += '~'
+  person.name += '~'
 }
 
 const changeAge = () => {
-  person.value.age++
+  person.age++
 }
 
 const changePerson = () => {
-  person.value = { name: '李四', age: 90 }
+  // ref的value实实在在的发生了变化，地址值是变了的
+  // reactive仅仅是，将其属性进行了修改，对象的地址值没有发生任何变化
+  Object.assign(person, { name: '李四', age: 90 })
 }
 
-// watch 监视ref定义的对象类型数据，监视的是对象的地址值，
-// 若想监视对象内部属性的变化，需要手动开启深度监视
-// watch 第一个参数，待监视的数据
-// 第二个参数，回调函数
-// 第三个参数，配置对象，（deep,immedidate等等)
+// watch 监视reactive定义的对象类型数据，且默认开启深度监视的，ref则需要手动开启
+// 该深度监视甚至无法关掉
 watch(
   person,
   (newValue, oldValue) => {
-    console.log('person变化', newValue, oldValue)
+    console.log('person变化了', newValue, oldValue)
   },
-  { deep: true, immediate: true }, //深度监视,立即执行一次回调函数，
+  { deep: false, immediate: true }, //深度监视,立即执行一次回调函数，
 )
 </script>
