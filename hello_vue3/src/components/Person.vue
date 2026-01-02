fd8d9b732d194142f1aabf5cf1bcef2eced6b902
Kuan_wsl2 <roumiankuan@outlook.com>
2026-01-02 16:15:17 +0800

    23,标签的ref属性

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="person">
-    <h2>需求：当水温达到60度或水位达到80cm时，给服务器发请求</h2>
-    <h2>当前水温：{{ temp }}°C</h2>
-    <h2>当前水位：{{ height }} cm</h2>
-    <button @click="changeTemp">点我水温+10</button>
-    <button @click="changeHeight">点我水位+10</button>
+    <h1>中国</h1>
+    <h2 ref="title2">北京</h2>
+    <h3>尚硅谷</h3>
+    <button @click="showLog">点我输出h2这个元素</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { watchEffect, ref } from 'vue'
+import { ref } from 'vue'
 
-let temp = ref(10)
-let height = ref(0)
+// 创建一个title2用于存储ref标记的内容
+// 操控原生的html DOM标签
+let title2 = ref()
 
-const changeTemp = () => {
-  temp.value += 10
+let a = ref(0)
+let b = ref(10)
+let c = ref(77)
+
+const showLog = () => {
+  console.log(title2.value)
 }
 
-const changeHeight = () => {
-  height.value += 10
-}
-// watchEffect与watch相比，watch需要亲口告诉它，需要监视谁，
-// 而watchEffect会自动分析，回调内使用了哪些响应式，自动的进行监视
-// 防止回调的函数体过大，要监视的数据太多，手写着麻烦
-// 而且watchEffect会自动触发一次，类似于immediate
-watchEffect(() => {
-  if (temp.value > 60 || height.value > 80) {
-    console.log('给服务器自动发送请求')
-  }
-})
+defineExpose({ a, b })
 </script>
