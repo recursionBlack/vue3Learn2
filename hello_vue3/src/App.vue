8a586ceb0a204cb50abeb306796f77d4a4d300c8
Kuan_wsl2 <roumiankuan@outlook.com>
2026-01-03 10:26:51 +0800

    31， 路由——基本切换效果

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,19 +1,69 @@
 <template>
-  <!-- html -->
-  <Person />
+  <div class="app" />
+  <h2 class="title">Vue路由测试</h2>
+  <!-- 导航区 -->
+  <div class="navigate">
+    <!-- RouterLink + to 实现按钮与路由路径的绑定 -->
+    <!-- active-class实现点击哪个按钮，哪个按钮亮 -->
+    <RouterLink to="/home" active-class="active">首页</RouterLink>
+    <RouterLink to="/news" active-class="active">新闻</RouterLink>
+    <RouterLink to="/about" active-class="active">关于</RouterLink>
+  </div>
+  <!-- 展示区 -->
+  <div class="main-content">
+    <!-- 命中的路由组件，放置在这里 -->
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <script lang="ts" setup>
-// js或ts
-import Person from './components/Person.vue'
+import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
 /* 样式 */
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
+.title {
+  text-align: center;
+  word-spacing: 5px;
+  margin: 30px 0;
+  height: 70px;
+  line-height: 70px;
+  background-image: linear-gradient(45deg, gray, white);
   border-radius: 10px;
-  padding: 20px;
+  box-shadow: 0 0 2px;
+  font-size: 30px;
+}
+.navigate {
+  display: block;
+  justify-content: space-around;
+  margin: 0 100px;
+}
+.navigate a {
+  display: block;
+  text-align: center;
+  width: 90px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  background-color: gray;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  letter-spacing: 5px;
+}
+.navigate a.active {
+  background-color: #64967e;
+  color: #ffc268;
+  font-weight: 900;
+  text-shadow: 0 0 1px black;
+  font-family: 微软雅黑;
+}
+.main-content {
+  margin: 0 auto;
+  margin-top: 30px;
+  border-radius: 10px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid;
 }
 </style>
---
+++ hello_vue3/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img src="https://www.atguigu.com/images/index_new/logo.png" alt="" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
