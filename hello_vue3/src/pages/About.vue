572defd77bee9e24cbb160ea90b32992860338ab
Kuan_wsl2 <roumiankuan@outlook.com>
2026-01-03 16:28:55 +0800

    32，路由——两个注意点

--- hello_vue3/src/pages/About.vue
+++ hello_vue3/src/pages/About.vue
@@ -4,7 +4,18 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+
+onMounted(() => {
+  console.log('被挂载了')
+})
+
+// 在路由切换时，被隐藏的组件是被卸载了
+onUnmounted(() => {
+  console.log('被卸载了')
+})
+</script>
 
 <style scoped>
 .about {
--- hello_vue3/src/router/index.ts
+++ hello_vue3/src/router/index.ts
@@ -2,9 +2,9 @@
 // 引入创建函数
 import { createRouter, createWebHistory } from 'vue-router'
 // 引入一个一个可能要呈现的组件
-import Home from '@/components/Home.vue'
-import News from '@/components/News.vue'
-import About from '@/components/About.vue'
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
 
 // 创建路由器
 const router = createRouter({
