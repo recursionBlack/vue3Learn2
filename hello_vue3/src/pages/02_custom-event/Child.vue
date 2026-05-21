fa9ec42dd393a1b4f26a36647a627d9abc638494
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-21 21:58:45 +0800

    53.组件通信_方式2_自定义事件

---
+++ hello_vue3/src/pages/02_custom-event/Child.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="child"">
+    <h3>子组件</h3>
+    <h4>玩具：{{ toy }}</h4>
+  </div>
+</template>
+
+<script setup lang="ts" name="child">
+import { ref, onMounted } from 'vue'
+// 数据
+let toy = ref('奥特曼')
+// 声明事件
+const emits =defineEmits(['send-toy'])
+// 自定义事件，主要用来从子类给父类传递数据，props父传子很方便，emit是子传父很方便，适合搭配使用
+
+// 组件挂载后3s，触发
+onMounted(()=>{
+    setTimeout(()=>{
+        emits('send-toy', toy)
+    }, 3000)
+})
+</script>
+
+<style scoped>
+.child {
+  margin-top: 10px;
+  background-color: rgb(76,209,76);
+  padding: 10px;
+  box-shadow: 0 0 10px black;
+  border-radius: 10px;
+}
+</style>
--- hello_vue3/src/router/index.ts
+++ hello_vue3/src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Props from '@/pages/01_props/Father.vue'
+import Event from '@/pages/02_cusstom-event/Father.vue'
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/props', component: Props }],
+  routes: [
+    { path: '/props', component: Props },
+    { path: '/event', component: Event },
+  ],
 })
