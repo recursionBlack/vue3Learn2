029e5e6ea933679d5b57b7bcc77c15d569711d2d
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-22 10:05:28 +0800

    55.组件通信_方式4_v-model

---
+++ hello_vue3/src/pages/04_v-model/AtguiguInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" :value="modelValue" @input="emits('update:modelValue', $event.target.value)" />
+</template>
+
+<script setup lang="en" name="AtguiguInput">
+defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+
+// 自己实现的组件库，通常由UI人员写。
+</script>
+
+<style scoped>
+.input {
+  border: 2px solid black;
+  background-image: linear-gradient(45deg, red, yellow, green);
+  height: 30px;
+  font-size: 20px;
+  color: white;
+}
+</style>
