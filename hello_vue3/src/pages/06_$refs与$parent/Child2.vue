b89a59e09bb1cdb20813b66d46deaa3952112815
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-22 16:17:19 +0800

    58.组件通信_方式6_$refs与$parent

---
+++ hello_vue3/src/pages/06_$refs与$parent/Child2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child2">
+    <h3>子组件2</h3>
+    <h4>电脑： {{ dell }}</h4>
+    <h4>书籍本：{{ book }}</h4>
+  </div>
+</template>
+
+<script setup lang="en" name="Child">
+import {ref} from 'vue'
+
+let computer = ref('dell')
+let book = ref(6)
+// 把数据暴露给外部
+defineExpose({computer,book})
+</script>
+
+<style scoped>
+.chlld2 {
+  margin-top: 20px;
+  background-color: orange;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px black;
+}
+</style>
