e32634f591d41f089d245f7e42f0bbcf3e05ad47
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-22 19:41:12 +0800

    61.插槽_默认插槽

---
+++ hello_vue3/src/pages/09_slot/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h3>子组件</h3>
+    <h2>{{ title }}</h2>
+    <!-- 承接插槽内容，模板形式实现的回调函数 -->
+    <slot>默认内容</slot>
+  </div>
+</template>
+
+<script setup lang="en" name="Category">
+defineProps(['title'])
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px black;
+  width: 200px;
+  height: 300px;
+}
+h2 {
+  background-color: orange;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
+}
+</style>
---
+++ hello_vue3/src/pages/09_slot/Father.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="father">
+    <h3>父组件</h3>
+    <div class="content">
+      <!-- 通常组件都是单标签，如果是双标签，则表示其中夹住的内容，是传递给子组件的内容，有点像回调函数 -->
+      <Categroy title="热门游戏列表">
+        <ul>
+          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+      </Categroy>
+      <Categroy title="今日美食城市">
+        <img :src="imgUrl" alt="" />
+      </Categroy>
+      <Categroy title="今日影视推荐">
+        <video :src="videoUrl" controls></video>
+      </Categroy>
+    </div>
+  </div>
+</template>
+
+<script setup lang="en" name="Father">
+import {ref, reactive,provide} from 'vue'
+import Categroy from './Category.vue'
+
+let games = reactive([{id:'qwerasdf01', name:'英雄联盟'},
+{id:'qwerasdf02', name:'王者农药'},
+{id:'qwerasdf03', name:'红色警戒'},
+{id:'qwerasdf04', name:'斗罗大陆'}])
+
+let imgUrl = ref('https://z1.ax1x.com/2023/11/19piNxLo4.jpg')
+
+let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
+</script>
+
+<style scoped>
+.father {
+  background-color: rgb(165, 164, 164);
+  padding: 20px;
+  border-radius: 10px;
+}
+
+/* 横向排列，并均分开 */
+.content {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+img,
+video {
+  width: 100%;
+  justify-content: space-evenly;
+}
+</style>
