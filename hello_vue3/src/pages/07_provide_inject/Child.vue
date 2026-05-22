afad89e424b6b7f7f756fb2f4e93d8f8a067cf52
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-22 16:46:28 +0800

    60.组件通信_方式7_provide_inject

---
+++ hello_vue3/src/pages/07_provide_inject/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    <h3>子组件</h3>
+    <h4>a: {{ a }}</h4>
+
+    <GrandChild />
+  </div>
+</template>
+
+<script setup lang="en" name="Child">
+import GrandChild from './GrandChild.vue';
+
+// 相比于$attrs，provide_inject的中间商不用参与任何修改，直接被绕过
+</script>
+
+<style scoped>
+.chlld {
+  margin-top: 20px;
+  background-color: skyblue;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px black;
+}
+</style>
