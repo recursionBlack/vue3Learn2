48824f9b2faba224f7dd9f5b08102f5fad888572
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-22 19:54:29 +0800

    62.插槽_具名插槽

--- hello_vue3/src/pages/09_slot/Father.vue
+++ hello_vue3/src/pages/09_slot/Father.vue
@@ -4,15 +4,21 @@
     <div class="content">
       <!-- 通常组件都是单标签，如果是双标签，则表示其中夹住的内容，是传递给子组件的内容，有点像回调函数 -->
       <Categroy title="热门游戏列表">
-        <ul>
-          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-        </ul>
+        <template v-slot:s2>
+          <ul>
+            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+          </ul>
+        </template>
+        <template v-slot:s1> <h2>热门游戏列表</h2></template>
       </Categroy>
-      <Categroy title="今日美食城市">
-        <img :src="imgUrl" alt="" />
+      <Categroy>
+        <template v-slot:s1> <h2>今日美食城市</h2></template>
+        <template v-slot:s2> <img :src="imgUrl" alt="" /></template>
       </Categroy>
-      <Categroy title="今日影视推荐">
-        <video :src="videoUrl" controls></video>
+      <Categroy>
+        <!-- #是语法糖，跟v-slot:功能一样 -->
+        <template #s2> <video :src="videoUrl" controls></video></template>
+        <template #s1> <h2>今日影视推荐</h2></template>
       </Categroy>
     </div>
   </div>
@@ -49,5 +55,12 @@ img,
 video {
   width: 100%;
   justify-content: space-evenly;
+}
+
+h2 {
+  background-color: orange;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
 }
 </style>
