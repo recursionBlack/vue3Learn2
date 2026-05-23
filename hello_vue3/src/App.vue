89ee6b6f6dea483cb260105a93620c8b592c1e5a
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-23 09:12:48 +0800

    第7章结束，清理掉相关代码

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,19 +1,5 @@
-<template>
-  <div class="container-fluid wraper">
-    <h1 class="title">Vue3 组件间通信</h1>
-    <hr />
-    <div class="row">
-      <div class="col-xs-3 col-md-3 col-xl-3">
-        <!-- 导航区 -->
-        <router-link active-class="active" class="list-group-item" to="/props">1.props</router-link>
-        <router-link active-class="active" class="list-group-item" to="/event">1.props</router-link>
-      </div>
-      <div class="col-xs-9 col-md-9 col-lg-9 col-xl-9">
-        <div class="panel-body"">
-            <!-- 占位一个展示区 -->
-             <router-view></router-view>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
+<template></template>
+
+<script></script>
+
+<style></style>
