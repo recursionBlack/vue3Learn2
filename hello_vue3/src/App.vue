6c654c6677ae4dc5fc451b1fdc01e38960377e7e
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-23 21:37:47 +0800

    69.suspense，一个vue3暂时试用的功能

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="outer">
+  <div class="app">
     <h2>我是App组件</h2>
-    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="" />
-    <br />
-    <Modal />
+    <Suspense>
+      <!-- 异步结果回来后显示该行 -->
+      <template v-slot:default>
+        <Child />
+      </template>
+      <!-- 异步结果暂未回来时显示该行 -->
+      <template v-slot:fallback>
+        <h3>加载中......</h3>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup lang="ts" name="App">
-import Modal from './Modal.vue'
-
-console.log(Modal)
+import { Suspense } from 'vue'
+import Child from './Child.vue'
 </script>
 
 <style>
-.outer {
+.app {
   background-color: #ddd;
   border-radius: 10px;
-  padding: 5px;
+  padding: 10px;
   box-shadow: 0 0 10px;
-  width: 400px;
-  height: 400px;
-}
-img {
-  width: 180px;
 }
 </style>
---
+++ hello_vue3/src/Child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="child">
+    <h2>我是子组件</h2>
+    <h3>当前求和为：{{ sum }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts" name="Child">
+import { ref } from 'vue'
+import axios from 'axios'
+
+let sum = ref(0)
+// 网络链接进不去
+let {
+  data: { content },
+} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+console.log(content)
+
+// suspense, 如果子组件内，的setup函数中，直接写了await 由于vue3中，无法自己写setup函数了，所以缺少了一个async,
+// 在该组件外部，就要使用suspense包裹，以表明该组件是一个异步组件
+</script>
+
+<style scoped>
+.child {
+  background-color: skyblue;
+  border-radius: 10px;
+  padding: 10px;
+  box-shadow: 0 0 10px;
+}
+</style>
