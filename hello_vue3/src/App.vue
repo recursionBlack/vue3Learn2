c7fd327af7359a5bd1c573a59d40834e6b8966b1
Kuan_wsl2 <roumiankuan@outlook.com>
2026-05-23 20:20:20 +0800

    第7章结束清理

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,20 +1,8 @@
 <template>
-  <div class="app">
-    <h2>{{ msg }}</h2>
-    <input type="text" v-model="msg" />
-  </div>
+  <div class="app"></div>
 </template>
 
-<script setup lang="ts" name="App">
-import { reactive, customRef } from 'vue'
-
-// 使用vue提供的默认ref定义响应式数据，数据一变化，界面立马更新
-// let msg = ref('你好')
-import useMsgRef from './useMsgRef'
-
-// 使用useMsgRef来定义一个响应式数据，且有一定的延时效果
-let { msg } = useMsgRef('尚硅谷', 2000)
-</script>
+<script setup lang="ts" name="App"></script>
 
 <style scoped>
 .app {
